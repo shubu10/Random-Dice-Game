3b3f1e070dd43cf7547598138c87d3afe230065f
Shubu <shubu.d10@gmail.com>
2022-06-04 02:25:06 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,18 @@
+var randomNumber1 = Math.floor(Math.random() * 6) + 1;
+var randomNumber2 = Math.floor(Math.random() * 6) + 1;
+
+var heading = document.querySelector(".container h1");
+
+document.querySelector(".img1").setAttribute("src", `images/dice${randomNumber1}.png`);
+document.querySelector(".img2").setAttribute("src", `images/dice${randomNumber2}.png`);
+
+
+if (randomNumber1 > randomNumber2){
+    heading.innerHTML = "Player 1 Wins!";
+}
+else if (randomNumber1 < randomNumber2){
+    heading.innerHTML = "Player 2 Wins!";
+}
+else{
+    heading.innerHTML = "Draw!";
+}
